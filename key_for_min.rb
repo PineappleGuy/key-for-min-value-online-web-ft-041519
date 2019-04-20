12a9af370bb4184edb89e37763d77cec6f3cfbc7
Garth Lilly <garff247@gmail.com>
2019-04-20 20:40:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  youngest_kid = 10000
-  if block_given?
-
+  youngest_kid = 1000
     name_hash.collect do |name, age|
         if age < youngest_kid
         youngest_kid = age
       end
   return name
     end
-
-  else
-  end
 end
