05426ab7c32bd36f3ddc2ba681c0c4846625b4c7
Garth Lilly <garff247@gmail.com>
2019-04-20 20:17:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  x = 0
+  if block_given?
+    while x >= value
+  name_hash.collect do |key, value|
+      key
+      x += 1
+    end
+  end
+  else
+    return nil
+  end
 end
